bf0d7bbb83d917c873b20a705bf2a7692fbd3547
danilo pineda <danilo.pineda@pevaar.com>
2021-01-31 23:29:13 -0500

    Add client and api for testing auth

---
+++ fm.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using IdentityModel.Client;
+using System.Net.Http;
+
+namespace fm.Console.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = client.GetDiscoveryDocumentAsync("https://localhost:5001").Result;
+            if (disco.IsError)
+            {
+                System.Console.WriteLine(disco.Error);
+                return;
+            }
+            var tokenResponse = client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "fmApiScope"
+            }).Result;
+
+            if (tokenResponse.IsError)
+            {
+                System.Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            System.Console.WriteLine(tokenResponse.Json);
+        }
+    }
+}
